4210e43f34ba0cf420de458d7c5a30283b500018
ouyx6 <839795368@qq.com>
2019-09-13 14:58:49 +0800

    函数方法实现的数据分类

---
+++ dicttest（函数）.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import sys
+
+output_dict = {}
+
+def handle_data(arg):
+    arg = arg.split(':')
+    a, b = arg
+    output_dict[a] = b
+
+def print_data(key, value):
+	print("ID:{} Name:{}".format(key, value))
+
+if __name__ == '__main__':
+
+    for arg in sys.argv[1:]:
+        handle_data(arg)
+    print(type(output_dict))
+    print(output_dict)
+    
+
+    for key in output_dict:
+        print_data(key, output_dict[key])
+
